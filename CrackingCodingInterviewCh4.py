5699ed42d2fcb9144b0d159c7161d72fd10b8c64
An Nguyen <anguye60@gmu.edu>
2022-06-16 00:35:11 -0400

    Update CrackingCodingInterviewCh4.py

--- CrackingCodingInterviewCh4.py
+++ CrackingCodingInterviewCh4.py
@@ -12,7 +12,7 @@ Gayle Laakmann McDowell
 # Given a direct graph, design an algorithm to find out whether there is a route between two nodes.
 
 """Breadth-First Search case"""
-from tkinter import E
+from tabnanny import check
 
 
 def routeFound(graph, node1, node2):
@@ -49,7 +49,7 @@ def routeFound(graph, node1, node2):
 def routeFoundDFS(graph, node1, node2):
     #If the first node is None, return False by definition
     if (node1 == None): 
-        return False
+        return False 
 
     #If the node references point to same node, return True by definition
     if (node1 == node2): 
@@ -70,9 +70,136 @@ def routeFoundDFS(graph, node1, node2):
     
     return False 
 
-
-    
-    return False
-
 # 4.2 - Minimal Tree
-# Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height. 
+# Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+
+class BinaryTreeNode: 
+    def __init__(self, value): 
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def getLeft(self):
+        return self.left
+    
+    def setLeft(self, node):
+        self.left = node
+    
+    def getRight(self):
+        return self.right
+    
+    def setRight(self, node):
+        self.right = node
+
+#This function is used to determine the start and end points of the given array
+def createMinimalTree(array):
+    start = 0 
+    end = len(array) - 1
+
+    return createMinimalTreeBFS(array, start, end)
+
+def createMinimalTreeBFS(array, start, end):
+    #If the ending is somehow smaller than start, return None
+    if (end < start): 
+        return None
+
+    middle = start + end / 2
+    value = array[middle] 
+
+    node = BinaryTreeNode(value)
+    node.setLeft(createMinimalTreeBFS(array, start, middle - 1))
+    node.setRight(createMinimalTreeBFS(array, middle + 1, end))
+
+    return node 
+
+
+# 4.3 - List of Depths
+# Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth 
+# That is, if there is a tree with depth D, there are D linked lists. 
+
+"""Depth-First Search Approach"""
+class LinkedListNode: 
+    def __init__(self, value): 
+        self.value = value
+        self.next = None
+    
+    def setNext(self, node): 
+        self.next = node
+    
+    def getNext(self):
+        return self.next
+    
+    def setValue(self, value):
+        self.value = value 
+
+    def getValue(self):
+        return self.value
+
+def createDepthLinkedLists(root, lists = [], level = 0):
+    #If the root node has no Node, then return None. This may also indicate the "parent" node is a leaf. 
+    #Base case
+    if (root == None): 
+        return None
+    
+    #If the number of lists equals the level, that means there is currently no linked list of that level
+    if (len(lists) == level): 
+        list = LinkedListNode(root.value)
+        lists.add(list)
+    else:
+        #Add the new node to the current linked list; traverse through linked list towards the end
+        list = lists[level]
+        while (list.getNext() != None): 
+            list = list.getNext()
+        
+        list.setNext(LinkedListNode(root.value))
+
+    #Depth-first search, using preorder traversal 
+    createDepthLinkedLists(root.getLeft(), lists, level + 1)
+    createDepthLinkedLists(root.getRight(), lists, level + 1)
+
+    return lists
+
+"""Breadth-First Search Approach"""
+
+# 4.4 - Check Balanced
+# Implement a function to check if a binary tree is balanced. 
+# DEFINITION for this question: A balanced tree is a tree such that the heights of the two subtrees of any node never differ more than one.
+
+def checkBalanced(node):
+    return checkHeight(node) != -2
+
+def checkHeight(node):
+    #This node is used for leaves as well as None nodes; this is the base case 
+    if (node == None): 
+        return -1
+
+    left = checkHeight(node.left)
+    right = checkHeight(node.right)
+
+    heightDiff = left - right
+    if (abs(heightDiff) > 1): 
+        #This is the value used to indicate the binary tree is not balanced
+        return -2  
+    else: 
+        return max(left, right) + 1  #Plus one is used to count the previous level (i.e. iterate height)
+
+# 4.5 - Validate BST
+# Implement a function to check if a binary tree is a binary search tree. 
+# For this problem, the definition is assumed to be: left_nodes <= current_node < right_nodes
+
+def validateBST(node): 
+    pass
+
+def checkBST(node): 
+    if (node == None): 
+        return
+    
+    leftVal = checkBST(node.left)
+    rightVal = checkBST(node.right) 
+
+    if (leftVal.val <= node.val) and (node.val < rightVal.val): 
+        return node
+    else: 
+        #This is the value to indicate that the binary tree is not a binary search tree
+        return
+
