fa515309cf979bf35109d89e15150316f975af94
An Nguyen <anguye60@gmu.edu>
2022-07-25 22:07:13 -0400

    Added Part 2

--- leetcodeBlind75.py
+++ leetcodeBlind75.py
@@ -215,7 +215,48 @@ class Solution(object):
                         left += 1
         
         return output
+
+# Search in Rotated Sorted Array
+# There is an integer array nums sorted in ascending order (with distinct values).
+# Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+# You must write an algorithm with O(log n) runtime complexity.     
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        left = 0 
+        right = len(nums) - 1
+        
+        while left <= right: 
+            mid = (left + right) // 2
+            
+            if (nums[mid] == target): 
+                return mid 
+            
+            # Check the left sorted portion  
+            if (nums[left] <= nums[mid]): 
+                if (target > nums[mid]) or (target < nums[left]):
+                    left = mid + 1
+                else: 
+                    right = mid - 1
+            
+            # Check the right sorted portion
+            else: 
+                if (target < nums[mid]) or (target > nums[right]):
+                    right = mid - 1
+                else: 
+                    left = mid + 1
                     
+        return -1 
+
+
+
+
 """LINKED LISTS"""
 # Reverse a Linked List
 # Given the head of a singly linked list, reverse the list, and return the reversed list.
@@ -273,368 +314,3 @@ class Solution(object):
             
         return False
 
-
-"TREES" 
-# Maximum Depth of Binary Tree
-
-# Given the root of a binary tree, return its maximum depth.
-# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        
-        if (root == None): 
-            return 0
-        
-        left = self.maxDepth(root.left) + 1
-        right = self.maxDepth(root.right) + 1
-        
-        return max(left, right) 
-
-# Same Tree 
-# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
-# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
-        
-        # If p and q are both None
-        if (not p) and (not q): 
-            return True
-        
-        # If p or q is None, then return False
-        if (not q) or (not p): 
-            return False
-        
-        # If the values do not match, return False
-        if p.val != q.val: 
-            return False
-        
-        # If values match, check the left and right subtrees to see if the same
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-
-# Invert Binary Tree
-# Given the root of a binary tree, invert the tree, and return its root.
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-        
-        # If root is Empty
-        if root == None: 
-            return
-        
-        temp = root.left
-        root.left = root.right
-        root.right = temp 
-        
-        self.invertTree(root.left)
-        self.invertTree(root.right) 
-        
-        return root 
-
-# Binary Tree Level Order Traversal 
-# Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        # If the root is None, return [] 
-        if root is None: 
-            return []
-        
-        queue = []
-        output = [] 
-        
-        # Add the root to the queue
-        queue.append(root) 
-    
-        # Loop through the queue as long as queue has elements...
-        while len(queue) != 0:
-            level = []
-            
-            for i in range(len(queue)): 
-    
-                # Remove the first element from the queue
-                item = queue.pop(0)
-                level.append(item.val) 
-
-                if item.left != None:
-                    queue.append(item.left) 
-                if item.right != None: 
-                    queue.append(item.right)
-            
-            # Ensures no empty levels are added
-            if level: 
-                output.append(level)
-        
-        return output 
-
-# Subtree of Another Tree
-# Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
-# A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def isSubtree(self, root, subRoot):
-        """
-        :type root: TreeNode
-        :type subRoot: TreeNode
-        :rtype: bool
-        """
-        
-        # If subroot is empty, then by definition it is a subroot of root
-        if (subRoot == None): 
-            return True
-        
-        # If root is empty, then the subroot has nothing to compare - return False
-        if (root == None): 
-            return False
-        
-        # If root.val == subRoot.val, and subRoot is a subtree of root, return True
-        if self.sameTree(root, subRoot): 
-            return True
-        
-        # If root.val != subRoot.val, check the left and right children to find a subtree
-        return self.isSubtree(root.left, subRoot.left) or self.isSubtree(root.right, subRoot.right)
-    
-    def sameTree(self, root, subRoot): 
-        if (root == None) and (subRoot == None): 
-            return True
-        
-        if (root) and (subRoot) and (root.val == subRoot.val):
-            return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right) 
-
-        return False
-
-# Validate Binary Search Tree  
-# Given the root of a binary tree, determine if it is a valid binary search tree (BST).
-# A valid BST is defined as follows:
-# 1) The left subtree of a node contains only nodes with keys less than the node's key.
-# 2) The right subtree of a node contains only nodes with keys greater than the node's key.
-# 3) Both the left and right subtrees must also be binary search trees.
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        
-        return self.validBST(root, float("-inf"), float("inf"))
-
-    def validBST(self, root, leftBoundary, rightBoundary):
-        
-        # Base case, an empty root is a valid BST by definition
-        if not root:
-            return True
-        
-        if not ((leftBoundary < root.val) and (root.val < rightBoundary)):
-            return False 
-        
-        return self.validBST(root.left, leftBoundary, root.val) and self.validBST(root.right, root.val, rightBoundary)    
-
-
-# Construct Binary Tree from Preorder and Inorder Traversal
-# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
-#
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: TreeNode
-        """
-        
-        # If either of the lists are empty, return None (no Nodes made)
-        if not (preorder) or not (inorder): 
-            return None
-    
-        # By definition, the first element of preorder list is the root
-        root = TreeNode(preorder[0])
-        
-        # Find index of root in the inorder, and split the inorder into two pieces
-        index = inorder.index(preorder[0])
-        
-        # Build the child nodes using the index information and the remaining lists
-        root.left = self.buildTree(preorder[1:index+1], inorder[:index])
-        root.right = self.buildTree(preorder[index+1:], inorder[index+1:])
-        
-        return root 
-
-# Binary Tree Maximum Path Sum
-# A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
-# The path sum of a path is the sum of the node's values in the path.
-# Given the root of a binary tree, return the maximum path sum of any non-empty path.
-#
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        maxSum = [root.val]
-    
-        # Calculate the max path sum without split
-        def calculatePath(root):
-            if not root: 
-                return 0 
-
-            leftSum = max(calculatePath(root.left), 0)
-            rightSum = max(calculatePath(root.right), 0)
-
-            # Compute max path sum with split 
-            maxSum[0] = max(maxSum[0], leftSum + rightSum + root.val)
-
-            return root.val + max(leftSum, rightSum)
-        
-        calculatePath(root)
-        return maxSum[0]
-
-"""STRING"""
-# Valid Parentheses
-# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-# 
-# An input string is valid if:
-# 1. Open brackets must be closed by the same type of brackets.
-# 2. Open brackets must be closed in the correct order.
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        
-        stack = [] 
-        
-        for element in s: 
-            if element in [')', '}', ']']:
-                if (len(stack) == 0): 
-                    return False
-                
-                stackElement = stack.pop()
-
-                if (element == ')' and stackElement != '('): 
-                    return False
-                elif (element == '}' and stackElement != '{'): 
-                    return False
-                elif (element == ']' and stackElement != '['): 
-                    return False
-            else: 
-                stack.append(element) 
-        
-        if (len(stack)): 
-            return False 
-        
-        return True
-
-# Valid Palindrome
-# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
-# Given a string s, return true if it is a palindrome, or false otherwise.
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-
-        # Set up the pointers
-        end = len(s) - 1
-        start = 0
-        
-        while (start < end): 
-            # Skip positions which characters are not alpahnumeric 
-            while (start < end) and (not self.isAlphaNum(s[start])):
-                start += 1
-            while (end > start) and (not self.isAlphaNum(s[end])): 
-                end -= 1
-            
-            # If the characters do not match, return False
-            if (s[start].lower() != s[end].lower()): 
-                return False
-            
-            # Iterate the pointers
-            start += 1
-            end -= 1
-        
-        return True
-            
-    def isAlphaNum(self, c): 
-        return (ord('A') <= ord(c) <= ord('Z')) or (ord('a') <= ord(c) <= ord('z')) or (ord('0') <= ord(c) <= ord('9'))
-
-"""Alternative Solution"""
-
-class Solution(object):
-    def isPalindrome(self, s):
-        newString = ""
-        
-        
-        # Remove all non-alphanumeric characters and lowercase the letters
-        for c in s: 
-            if c.isalnum(): 
-                newString += c.lower() 
-        
-        return newString == newString[::-1]
-    
