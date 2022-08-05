ba0e6cd2598384dc10aa0b1372a04061770490c5
An Nguyen <anguye60@gmu.edu>
2022-08-05 00:02:20 -0400

    Update leetcodeBlind75part2.py

--- leetcodeBlind75part2.py
+++ leetcodeBlind75part2.py
@@ -505,7 +505,7 @@ class Solution(object):
  # Group Anagrams
  # Given an array of strings strs, group the anagrams together. You can return the answer in any order.
  # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
- class Solution(object):
+class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
@@ -527,4 +527,35 @@ class Solution(object):
             listAnagrams[tuple(count)].append(string)
             
         return listAnagrams.values()
+
+# Longest Repeating Character Replacement
+# You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+# Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
         
+        # Get the counts of the each character in the string s
+        count = {} 
+        result = 0
+        
+        left = 0
+        maxFreq = 0 
+        for right in range(len(s)): 
+            count[s[right]] = 1 + count.get(s[right], 0)
+            maxFreq = max(maxFreq, count[s[right]])
+            
+            # If the number of replacements is greater than k, shift left
+            while (right - left + 1) - maxFreq > k: 
+                count[s[left]] -= 1
+                left += 1
+            
+            result = max(result, right - left + 1)
+        
+        # Return the result (maximum length) of the replacements
+        return result
