559d255c9828e6ea53bd181883154a12ad0f5f0a
An Nguyen <anguye60@gmu.edu>
2022-08-07 17:07:31 -0400

    Create leetcodeBlind75part4.py

---
+++ leetcodeBlind75part4.py
@@ -0,0 +1,157 @@
+
+"""MATRIX"""
+
+# Spiral Matrix
+
+# Given an m x n matrix, return all elements of the matrix in spiral order.
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [1,2,3,6,9,8,7,4,5]
+# 
+# Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+# Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        
+        result = []
+
+        # Row
+        top = 0 
+        bottom = len(matrix)   
+        
+        # Column 
+        left = 0
+        right = len(matrix[0])  
+        
+        while (left < right) and (top < bottom): 
+            
+            # Get values from top row, left to right
+            for i in range(left, right): 
+                result.append(matrix[top][i])
+            top += 1  # Shift top by 1 (eliminating row just completed)
+            
+            # Get values from rightmost column, top to bottom
+            for i in range(top, bottom): 
+                result.append(matrix[i][right - 1])   #-1 to prevent out of bounds
+            right -= 1 
+            
+            # This is used for the middle the matrix 
+            if not (left < right and top < bottom): 
+                break 
+
+            # Get values from bottom row, right to left
+            for i in range(right - 1, left - 1, -1): 
+                result.append(matrix[bottom - 1][i]) 
+            bottom -= 1
+            
+            # Get values from leftmost column, bottom to top 
+            for i in range(bottom - 1, top - 1, -1): 
+                result.append(matrix[i][left])
+            left += 1
+        
+        return result          
+            
+# Word Search 
+# Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+# The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+#
+# EXAMPLE: 
+# Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+# Output: true
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        
+        ROWS = len(board)
+        COLS = len(board[0])
+        
+        # Add current values from board that currently in path
+        path = set() 
+        
+        def dfs(row, col, i):
+
+            # If index i is length of word, word has been found
+            if i == len(word): 
+                return True
+
+            # If index is out of bounds, or index at word does not match the board, or row/col combination is already in path
+            if (row < 0 or col < 0 or 
+                row >= ROWS or col >= COLS or 
+                word[i] != board[row][col] or 
+                (row, col) in path): 
+                return False
+            
+            path.add((row, col))
+            
+            # Find the target word continuously at next index
+            result = (dfs(row+1, col, i + 1) or 
+                        dfs(row-1, col, i + 1) or 
+                        dfs(row, col+1, i + 1) or 
+                        dfs(row, col-1, i + 1))
+            
+            # Remove row, col combination once backtracking 
+            path.remove((row, col))
+            
+            return result 
+        
+        for r in range(ROWS): 
+            for c in range(COLS): 
+                if dfs(r, c, 0): return True
+        return False
+
+# Rotate Image 
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+#
+# EXAMPLE: 
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[7,4,1],[8,5,2],[9,6,3]]
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        # Build the pointers
+        left = 0
+        right = len(matrix) - 1
+        
+        # Left and right pointers may not cross
+        while (left < right):
+            for i in range(right - left):
+                
+                # This is a square matrix pointers
+                top = left
+                bottom = right
+            
+                # Save the leftmost top value
+                topLeft = matrix[top][left + i] 
+                
+                # Move bottom left to top left
+                matrix[top][left + i] = matrix[bottom - i][left]
+                
+                # Move bottom right to bottom left
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+                
+                # Move top right to bottom right
+                matrix[bottom][right - i] = matrix[top + i][right]
+                
+                # Move top left to top right using temp variable
+                matrix[top + i][right] = topLeft
+        
+            # Update the left and right pointers to do submatrix 
+            left += 1
+            right -= 1
+                 
+                        
+                        
+                        
+                        
+            
